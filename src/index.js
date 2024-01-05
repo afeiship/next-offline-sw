e902346517f2843a17640ad51b0d5621b85d4103
afeiship <1290657123@qq.com>
2024-01-05 13:33:09 +0800

    feat: new scripts

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
-(function () {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var NAV = global.navigator;
-  var runtime = require('offline-plugin/runtime');
-  var DEFAULT_OPTIONS = {
-    onUpdateReady: function () {
-      console.warn(
-        'SW Event:',
-        'You need to call `nx.OfflineSw.applyUpdate` to update resource when [onUpdated].'
-      );
-    }
-  };
+import nx from '@jswork/next';
 
-  var NxOfflineSw = nx.declare('nx.OfflineSw', {
-    statics: {
-      runtime: runtime,
-      disabled: function () {
-        return global.__SW_DISABLED__;
-      },
-      install: function (inOptions) {
-        if (this.disabled()) return;
-        var options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
-        runtime.install(options);
-      },
-      uninstall: function () {
-        NAV.serviceWorker.getRegistrations().then(function (registrations) {
-          registrations.forEach(function (registration) {
-            registration.unregister();
-          });
-        });
-      },
-      update: function (inDelay) {
-        var self = this;
-        var delay = inDelay || 300;
-        this.applyUpdate();
-        setTimeout(function () {
-          self.refresh();
-        }, delay);
-      },
-      refresh: function () {
-        global.location.reload();
-      },
-      applyUpdate: function () {
-        runtime.applyUpdate();
-      }
-    }
-  });
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = NxOfflineSw;
+const NAV = global.navigator;
+const runtime = require('offline-plugin/runtime');
+const DEFAULT_OPTIONS = {
+  onUpdateReady: function () {
+    console.warn(
+      'SW Event:',
+      'You need to call `nx.OfflineSw.applyUpdate` to update resource when [onUpdated].'
+    );
   }
-})();
+};
+
+const NxOfflineSw = nx.declare('nx.OfflineSw', {
+  statics: {
+    runtime: runtime,
+    disabled: function () {
+      return global.__SW_DISABLED__;
+    },
+    install: function (inOptions) {
+      if (this.disabled()) return;
+      const options = nx.mix(null, DEFAULT_OPTIONS, inOptions);
+      runtime.install(options);
+    },
+    uninstall: function () {
+      NAV.serviceWorker.getRegistrations().then(function (registrations) {
+        registrations.forEach(function (registration) {
+          registration.unregister();
+        });
+      });
+    },
+    update: function (inDelay) {
+      const self = this;
+      const delay = inDelay || 300;
+      this.applyUpdate();
+      setTimeout(function () {
+        self.refresh();
+      }, delay);
+    },
+    refresh: function () {
+      global.location.reload();
+    },
+    applyUpdate: function () {
+      runtime.applyUpdate();
+    }
+  }
+});
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = NxOfflineSw;
+}
+
+export default NxOfflineSw;
